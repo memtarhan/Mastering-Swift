701312b028408551c0ed346493e65ba0b8f19aa3
Mehmet Tarhan <hi@memtarhan.com>
2022-04-01 18:30:48 +0100

    [Delegations and Data Sources] - Data Sources

--- 1-delegations-and-data-sources/MyPlayground.playground/Contents.swift
+++ 1-delegations-and-data-sources/MyPlayground.playground/Contents.swift
@@ -1,32 +1,37 @@
 import PlaygroundSupport
 import UIKit
 
-class Colors {
+class Theme {
+    weak var dataSource: ColorChooserDataSource?
+
     var index = 0
-    private let names = ["red", "green", "blue", "purple"]
-    private let colors = [UIColor.red, .green, .blue, .purple]
 
-    func getName(_ index: Int) -> String {
-        return names[index % names.count]
+    func getName(_ index: Int) -> String? {
+        self.index = index % (dataSource?.numberOfColors() ?? 1)
+        return dataSource?.nameOfColor(for: self.index)
     }
 
-    func getColor(_ index: Int) -> UIColor {
-        return colors[index % colors.count]
+    func getColor(_ index: Int) -> UIColor? {
+        self.index = index % (dataSource?.numberOfColors() ?? 1)
+        return dataSource?.colorOfColor(for: self.index)
     }
-
-    var allNames: [String] { names }
-    var count: Int { names.count }
 }
 
-class ViewController: UIViewController, ColorChooserDelegate {
+class ViewController: UIViewController, ColorChooserDelegate, ColorChooserDataSource {
+    let theme = Theme()
+
     var count = 0
-    var colors = Colors()
     let button = UIButton(frame: CGRect(x: 0, y: 0, width: 200, height: 44))
+
+    private let names = ["red", "green", "blue", "purple"]
+    private let colors = [UIColor.red, .green, .blue, .purple]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = colors.getColor(count)
+        theme.dataSource = self
+
+        view.backgroundColor = colorOfColor(for: count)
 
         button.setTitle("Color Choice", for: .normal)
         button.backgroundColor = .darkGray
@@ -34,6 +39,10 @@ class ViewController: UIViewController, ColorChooserDelegate {
         button.center = view.center
         button.addTarget(self, action: #selector(changeColor(sender:)), for: .touchUpInside)
         view.addSubview(button)
+    }
+
+    deinit {
+        theme.dataSource = nil
     }
 
     @IBAction func changeColor(sender: UIButton) {
@@ -45,31 +54,53 @@ class ViewController: UIViewController, ColorChooserDelegate {
 
     func colorChooser(didChooseColor color: Int) {
         count = color
-        view.backgroundColor = colors.getColor(count)
+        view.backgroundColor = colorOfColor(for: count)
+    }
+
+    func numberOfColors() -> Int {
+        return names.count
+    }
+
+    func nameOfColor(for index: Int) -> String {
+        return names[index]
+    }
+
+    func colorOfColor(for index: Int) -> UIColor {
+        return colors[index]
     }
 }
 
-class ColorChooserViewController: UIViewController {
-    var colors = Colors()
+class ColorChooserViewController: UIViewController, ColorChooserDataSource {
+    let theme = Theme()
+
     var currentColor = 0
+
+    private let names = ["red", "green", "blue", "purple"]
+    private let colors = [UIColor.red, .green, .blue, .purple]
 
     weak var delegate: ColorChooserDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let chooser = UISegmentedControl(items: colors.allNames)
+        theme.dataSource = self
+
+        let chooser = UISegmentedControl(items: names)
         chooser.frame = CGRect(x: 20, y: 0, width: view.frame.width - 40, height: 44)
         chooser.addTarget(self, action: #selector(choose(_:)), for: .valueChanged)
         chooser.tintColor = .black
         view.addSubview(chooser)
 
-        view.backgroundColor = colors.getColor(currentColor)
+        view.backgroundColor = colorOfColor(for: currentColor)
+    }
+
+    deinit {
+        theme.dataSource = nil
     }
 
     @objc func choose(_ sender: UISegmentedControl) {
         currentColor = sender.selectedSegmentIndex
-        let color = colors.getColor(currentColor)
+        let color = colorOfColor(for: currentColor)
         view.backgroundColor = color
 
         delegate?.colorChooser(didChooseColor: currentColor)
@@ -78,10 +109,28 @@ class ColorChooserViewController: UIViewController {
             self.delegate = nil
         }
     }
+
+    func numberOfColors() -> Int {
+        return names.count
+    }
+
+    func nameOfColor(for index: Int) -> String {
+        return names[index]
+    }
+
+    func colorOfColor(for index: Int) -> UIColor {
+        return colors[index]
+    }
 }
 
 protocol ColorChooserDelegate: AnyObject {
     func colorChooser(didChooseColor color: Int)
+}
+
+protocol ColorChooserDataSource: AnyObject {
+    func numberOfColors() -> Int
+    func nameOfColor(for index: Int) -> String
+    func colorOfColor(for index: Int) -> UIColor
 }
 
 PlaygroundPage.current.liveView = ViewController()
