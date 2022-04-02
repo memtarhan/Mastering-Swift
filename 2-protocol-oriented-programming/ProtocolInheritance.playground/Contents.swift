18fbc5583cd2995c03a83275ef33a287a3bcb154
Mehmet Tarhan <hi@memtarhan.com>
2022-04-02 16:59:30 +0100

    [Protocol Oriented Programming] - Adopting multiple protocols

--- 2-protocol-oriented-programming/ProtocolInheritance.playground/Contents.swift
+++ 2-protocol-oriented-programming/ProtocolInheritance.playground/Contents.swift
@@ -18,3 +18,35 @@ protocol TaggedPersistable: Taggable, CustomStringConvertible, Equatable {
 protocol TaggedEncodable: Taggable {
     var base64: String { get }
 }
+
+// MARK: - Adopting multiple protocols
+
+struct MyData: Taggable {
+    var tag: String
+
+    var data: Data
+
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+}
+
+extension MyData: CustomStringConvertible {
+    var description: String { "MyData\(tag)" }
+}
+
+extension MyData: TaggedEncodable {
+    var base64: String { data.base64EncodedString() }
+}
+
+extension MyData: TaggedPersistable {
+    init(tag: String, contentsOf url: URL) throws {
+        let data = try Data(contentsOf: url)
+        self.init(tag: tag, data: data)
+    }
+
+    func persist(to url: URL) throws {
+        try data.write(to: url)
+    }
+}
