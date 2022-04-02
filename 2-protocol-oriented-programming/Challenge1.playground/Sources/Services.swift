7d142e4295709d1908c63bed0bdcdc7215fbfb6d
Mehmet Tarhan <hi@memtarhan.com>
2022-04-02 17:28:43 +0100

    [Protocol Oriented Programming] - Challenge 1

---
+++ 2-protocol-oriented-programming/Challenge1.playground/Sources/Services.swift
@@ -0,0 +1,62 @@
+import Foundation
+
+public protocol Service {
+    var total: Float { get }
+
+    mutating func add(payment: Float)
+}
+
+private class AmazonService {
+    private var balance: Float = 0
+
+    fileprivate init() {}
+
+    private func orderPlaced(payment: Float) {
+        balance += payment
+    }
+
+    private var earnings: Float { balance }
+}
+
+extension AmazonService: Service {
+    public var total: Float {
+        earnings
+    }
+
+    public func add(payment: Float) {
+        orderPlaced(payment: payment)
+    }
+}
+
+private class EtsyService {
+    private var earnings: Float = 0
+
+    fileprivate init() {}
+
+    private func itemSold(profit: Float) {
+        earnings += profit
+    }
+
+    private var totalSold: Float { earnings }
+}
+
+extension EtsyService: Service {
+    public var total: Float {
+        totalSold
+    }
+
+    public func add(payment: Float) {
+        itemSold(profit: payment)
+    }
+}
+
+public enum ServiceType {
+    case amazon, etsy
+}
+
+public func make(service: ServiceType) -> Service {
+    switch service {
+    case .amazon: return AmazonService()
+    case .etsy: return EtsyService()
+    }
+}
