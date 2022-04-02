d040558f0091eecabc63e5a0d263a990f35153ef
Mehmet Tarhan <hi@memtarhan.com>
2022-04-02 17:05:46 +0100

    [Protocol Oriented Programming] - Protocol extensions

--- 2-protocol-oriented-programming/ProtocolInheritance.playground/Contents.swift
+++ 2-protocol-oriented-programming/ProtocolInheritance.playground/Contents.swift
@@ -15,13 +15,28 @@ protocol TaggedPersistable: Taggable, CustomStringConvertible, Equatable {
     func persist(to url: URL) throws
 }
 
+extension TaggedPersistable {
+    init(tag: String, contentsOf url: URL) throws {
+        let data = try Data(contentsOf: url)
+        self.init(tag: tag, data: data)
+    }
+
+    func persist(to url: URL) throws {
+        try data.write(to: url)
+    }
+}
+
 protocol TaggedEncodable: Taggable {
     var base64: String { get }
 }
 
+extension TaggedEncodable {
+    var base64: String { data.base64EncodedString() }
+}
+
 // MARK: - Adopting multiple protocols
 
-struct MyData: Taggable {
+struct MyData: TaggedPersistable, TaggedEncodable {
     var tag: String
 
     var data: Data
@@ -36,17 +51,20 @@ extension MyData: CustomStringConvertible {
     var description: String { "MyData\(tag)" }
 }
 
-extension MyData: TaggedEncodable {
-    var base64: String { data.base64EncodedString() }
-}
+// MARK: - Protocol extensions
 
-extension MyData: TaggedPersistable {
-    init(tag: String, contentsOf url: URL) throws {
-        let data = try Data(contentsOf: url)
-        self.init(tag: tag, data: data)
+struct PersistableData: TaggedPersistable {
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
     }
 
-    func persist(to url: URL) throws {
-        try data.write(to: url)
-    }
+    var tag: String
+
+    var data: Data
+
+    var description: String { "PersistableData\(tag)" }
 }
+
+let p = PersistableData(tag: "42", data: Data(repeating: 1, count: 10))
+//try p.persist(to: URL(string: "")!)
