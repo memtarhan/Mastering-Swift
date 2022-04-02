101024db3d7d46ff579176d392926150d67bb11f
Mehmet Tarhan <hi@memtarhan.com>
2022-04-02 23:54:12 +0100

    [Protocol Oriented Programming] - Completing the OpenWeatherMap controller

--- 2-protocol-oriented-programming/WeatherApp/WeatherApp/Model/OpenWeatherMapController.swift
+++ 2-protocol-oriented-programming/WeatherApp/WeatherApp/Model/OpenWeatherMapController.swift
@@ -21,5 +21,36 @@ class OpenWeatherMapController: WebServiceController {
             completionHandler(nil, .invalidURL(endpoint))
             return
         }
+
+        let dataTask = URLSession.shared.dataTask(with: endpointURL) { data, _, error in
+            guard error == nil else {
+                completionHandler(nil, .forwarded(error!))
+                return
+            }
+
+            guard let responseData = data else {
+                completionHandler(nil, .invalidPayload(endpointURL))
+                return
+            }
+
+            let decoder = JSONDecoder()
+            do {
+                let weatherList = try decoder.decode(OpenWeatherMapContainer.self, from: responseData)
+                guard let weatherInfo = weatherList.list?.first,
+                      let weather = weatherInfo.weather.first?.main,
+                      let temperature = weatherInfo.main.temp else {
+                    completionHandler(nil, .invalidPayload(endpointURL))
+                    return
+                }
+
+                let weatherDescription = "\(weather) \(temperature) C"
+                completionHandler(weatherDescription, nil)
+
+            } catch {
+                completionHandler(nil, .forwarded(error))
+            }
+        }
+
+        dataTask.resume()
     }
 }
---
+++ 2-protocol-oriented-programming/WeatherApp/WeatherApp/Model/OpenWeatherMapResponse.swift
@@ -0,0 +1,28 @@
+//
+//  OpenWeatherMapResponse.swift
+//  WeatherApp
+//
+//  Created by Mehmet Tarhan on 02/04/2022.
+//
+
+import Foundation
+
+struct OpenWeatherMapContainer: Codable {
+    var list: [OpenMapWeatherData]?
+}
+
+struct OpenMapWeatherData: Codable {
+    var weather: [OpenMapWeatherWeather]
+    var main: OpenMapWeatherMain
+}
+
+struct OpenMapWeatherWeather: Codable {
+    var id: Int?
+    var main: String?
+    var description: String?
+    var icon: String?
+}
+
+struct OpenMapWeatherMain: Codable {
+    var temp: Float?
+}
