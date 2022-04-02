1bee4c0230eed8f6899749d649434761f43e7de4
Mehmet Tarhan <hi@memtarhan.com>
2022-04-02 23:21:11 +0100

    [Protocol Oriented Programming] - Created WeatherApp

---
+++ 2-protocol-oriented-programming/WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Mehmet Tarhan on 02/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
