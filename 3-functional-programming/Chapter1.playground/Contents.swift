fca12ab5563948dee2a37d2fa6ffa44c4e17d4b5
Mehmet Tarhan <hi@memtarhan.com>
2022-04-05 00:32:08 +0100

    [Functional Programming] - Chapter 1

---
+++ 3-functional-programming/Chapter1.playground/Contents.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+// MARK: What Is Functional Programming?
+
+/*
+ - Another programming paradigm similar to imperative programming, object oriented programming and logic programming
+ - It represents a way of organizing the large number of complex ideas present in a computer program in a coherent way.
+
+ /// 3 Concepts of FP
+ - Seperating functions and data
+ - Immutability
+ - First-class functions
+
+ */
+
+// MARK: - Immutability
+
+/*
+ In functional programming, we don't assign, we define and it's a constant.
+ */
+
+// MARK: - Seperating functions and data
+
+/*
+
+ */
+
+// MARK: - First-class functions
+
+/*
+
+ */
